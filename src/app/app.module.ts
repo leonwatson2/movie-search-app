48f645ee3b27875096d215fb263035dba2f46de5
vlw0052 <leon@vlw2.com>
2017-11-07 15:07:36 -0600

    Added HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { DisplayMovieComponent } from './display-movie.component';
@@ -15,7 +16,8 @@ import { MovieService } from './movie.service'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
